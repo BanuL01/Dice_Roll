d7174b22a324a2a82eec7dd25d4428671da97716
Banuji Liyanwala <banuji01liyanwala@gmail.com>
2023-03-15 12:52:17 +0530

    about page design

--- app/src/main/java/com/example/dice_application/MainActivity.kt
+++ app/src/main/java/com/example/dice_application/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.dice_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val next_button : Button = findViewById(R.id.Next)
+//        next_button.setOnClickListener {
+//            val intent = Intent(this, options::class.java)
+//            startActivity(intent)
+//
+//            }
+        // Next page connect
+        //        //source - https://www.geeksforgeeks.org/how-to-create-a-splash-screen-in-android-using-kotlin/
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, options::class.java)
+            startActivity(intent)
+            finish()
+        },2500)
+
     }
 }
--- app/src/main/java/com/example/dice_application/options.kt
+++ app/src/main/java/com/example/dice_application/options.kt
@@ -1,11 +1,29 @@
 package com.example.dice_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 
 class options : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+//        val new_game_button : Button = findViewById(R.id.new_game)
+//        new_game_button.setOnClickListener {
+//            val intent = Intent(this, about::class.java)
+//            startActivity(intent)
+//        }
+
+        val about_button : Button = findViewById(R.id.about)
+
+        about_button.setOnClickListener {
+            val intent = Intent(this, about::class.java)
+            startActivity(intent)
+        }
+
     }
 }
